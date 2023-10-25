38738dadc0d18b65e2188ebbc5dcbbfddb316652
Joao <guga67423@outlook.com>
2023-10-25 12:33:33 -0300

    organized script and created function for inserting a paragraph

--- ex001/script.js
+++ ex001/script.js
@@ -1,11 +1,23 @@
-const title = "This Is My Title"
+//function for console loging title
+const insertTitle = (title) => {
+    
+    var boxWidth = ""
 
-var boxWidth = ""
+    for (var i = 0; i < title.length; i++){
+        boxWidth += " "
+    }
 
-for (var i = 0; i < title.length; i++){
-    boxWidth += " "
+    console.log("+ " + boxWidth + " +")
+    console.log("- " + title + " -")
+    console.log("+ " + boxWidth + " +")
+
 }
 
-console.log("+ " + boxWidth + " +")
-console.log("- " + title + " -")
-console.log("+ " + boxWidth + " +")
+//function for console loging a paragraph
+const insertParagraph = (text) => {
+    console.log(text)
+}
+
+insertTitle("Aurora | Find Artists App")
+insertParagraph("Aurora is an application for find your favorite artists and know more about them!")
+console.log("worked")
