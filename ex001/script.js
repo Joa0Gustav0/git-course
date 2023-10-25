546fe8ce88e29e1ffa8192dbd955dd1498d050d5
Joao <guga67423@outlook.com>
2023-10-25 12:22:06 -0300

    created script for console loging required titles

---
+++ ex001/script.js
@@ -0,0 +1,11 @@
+const title = "This Is My Title"
+
+var boxWidth = ""
+
+for (var i = 0; i < title.length; i++){
+    boxWidth += " "
+}
+
+console.log("+ " + boxWidth + " +")
+console.log("- " + title + " -")
+console.log("+ " + boxWidth + " +")
